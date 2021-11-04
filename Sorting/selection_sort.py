f13599c50daffe436199d667f9dc312adba36ccc
Steven Samy <stevensivo27@gmail.com>
2021-11-04 17:53:18 +0200

    selection sort in python

--- Sorting/selection_sort.py
+++ Sorting/selection_sort.py
@@ -1,7 +1,20 @@
- def selection_sort(A):                      # Selection sort array A
-    for i in range(len(A) - 1, 0, -1):       # O(n) loop over array
+def selection_sort(A):                       # Selection sort array A
+    for i in range(len(A) - 1):              # O(n) loop over array
         m = i                                # O(1) initial index of max
-        for j in range(i):                   # O(i) search for max in A[:i]
-            if A[m] < A[j]:                  # O(1) check for larger value
+        for j in range(i+1, len(A)-1):       # O(i) search for max in A[:i]
+            if A[j] < A[m]:                  # O(1) check for larger value
                 m = j                        # O(1) new max found
         A[m], A[i] = A[i], A[m]              # O(1) swap
+
+#E.X
+A = [3,2,1,4]
+print(A)
+selection_sort(A)
+print(A)
+
+# OUTPUT
+'''
+[3, 2, 1, 4]
+[1, 2, 3, 4]
+
+'''
